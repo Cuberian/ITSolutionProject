a3fb9745bbd8343989d81142248b3bad77b322d8
ismakova <ismakova.dayana@mail.ru>
2020-10-21 21:35:19 +0500

    новая функция

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -10,5 +10,8 @@ use Illuminate\Routing\Controller as BaseController;
 class Controller extends BaseController
 {
     use AuthorizesRequests, DispatchesJobs, ValidatesRequests;
+    function hi(){
+        return "hi";
+    }
 }
 
