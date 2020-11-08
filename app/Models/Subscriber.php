70083de2a696860f137325862c0797ffc294c61a
Augen-Auf <an_lesiv@mail.ru>
2020-11-08 19:20:27 +0500

    fixed grammar error

--- app/Models/Subscriber.php
+++ app/Models/Subscriber.php
@@ -8,6 +8,6 @@ use Illuminate\Database\Eloquent\Model;
 class Subscriber extends Model
 {
     use HasFactory;
-    protected $table = 'subcribers';
+    protected $table = 'subscribers';
     protected $fillable = ['user_id', 'group_id', 'is_admin'];
 }
