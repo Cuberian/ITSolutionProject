af2cf6337b88c100f6f49b093af010bfeb3a05e8
Augen-Auf <an_lesiv@mail.ru>
2020-11-15 15:23:04 +0500

    added controllers

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,7 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
+Route::prefix('admin')->group(function () {
+    Route::get('users', function () {
+    });
 });
